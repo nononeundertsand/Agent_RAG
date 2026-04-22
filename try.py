f3aad2757e0bed9bc86286a43869de06491a0452
XiaoWei Liu <1821986277@qq.com>
2026-04-22 21:10:59 +0800

    1

--- try.py
+++ try.py
@@ -1,8 +1,8 @@
 from qdrant_client import QdrantClient
 
 client = QdrantClient(
-    url="https://287dba7c-88ff-4e65-8fb9-6d0b2af5ff82.eu-central-1-0.aws.cloud.qdrant.io:6333",
-    api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.-a3rJYYRZNeo07kENdn6mWoy_Ys8jR_KwrJ056LWe3k"
+    url="",
+    api_key=""
 )
 
 print(client.get_collections())
